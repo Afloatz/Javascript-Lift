97f2d8a54da7193780dfd6fc36028797cc319355
Afloatz <florentlaot.fr@gmail.com>
2019-08-07 16:53:35 +0200

    self-executing function

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,63 +1,82 @@
-// Create the object RepLopApp
-var RepLogApp = {
-    initialize: function ($wrapper) {
-        this.$wrapper = $wrapper;
+(function () {
+    
+    // Create the object RepLopApp
+    var RepLogApp = {
+        initialize: function ($wrapper) {
+            this.$wrapper = $wrapper;
+            Helper.initialize($wrapper);
 
-        this.$wrapper.find('.js-delete-rep-log').on(
-            'click',
-            this.handleRepLogDelete.bind(this)
-        );
+            this.$wrapper.find('.js-delete-rep-log').on(
+                'click',
+                this.handleRepLogDelete.bind(this)
+            );
 
-        this.$wrapper.find('tbody tr').on(
-            'click',
-            this.handleRawClick.bind(this)
-        );
-    },
+            this.$wrapper.find('tbody tr').on(
+                'click',
+                this.handleRawClick.bind(this)
+            );
+        },
 
-    updateTotalWeightLifted: function () {
-        var totalWeight = 0;
-        this.$wrapper.find('tbody tr').each(function () {
-            totalWeight += $(this).data('weight');
+        updateTotalWeightLifted: function () {
+            this.$wrapper.find('.js-total-weight').html(
+                Helper.calculateTotalWeight()
+            );
+        },
 
-        });
+        handleRepLogDelete: function (e) {
+            // To avoid going back at the top of the page because of the html tag <a href="#">
+            e.preventDefault();
 
-        this.$wrapper.find('.js-total-weight').html(totalWeight);
-    },
+            var $link = $(e.currentTarget);
+            // To put the Delete in red, keyword this refers to the clicked element
+            $link.addClass('text-danger');
 
-    handleRepLogDelete: function (e) {
-        // To avoid going back at the top of the page because of the html tag <a href="#">
-        e.preventDefault();
+            // To have a rotating spinner instead of the trash when clicking on delete
+            $link.find('.fa')
+                .removeClass('fa-trash')
+                .addClass('fa-spinner')
+                .addClass('fa-spin');
 
-        var $link = $(e.currentTarget);
-        // To put the Delete in red, keyword this refers to the clicked element
-        $link.addClass('text-danger');
+            // To delete the entire row
+            // .data() is a shortcut to read a data attribute
+            var deleteUrl = $link.data('url');
+            var $row = $link.closest('tr');
+            // Use of the variable self to ensure that in our callback function success, "this" is the object RepLogApp
+            var self = this;
+            $.ajax({
+                url: deleteUrl,
+                method: 'DELETE',
+                success: function () {
+                    $row.fadeOut('normal', function () {
+                        $(this).remove();
+                        self.updateTotalWeightLifted();
 
-        // To have a rotating spinner instead of the trash when clicking on delete
-        $link.find('.fa')
-            .removeClass('fa-trash')
-            .addClass('fa-spinner')
-            .addClass('fa-spin');
+                    });
+                }
+            });
+        },
 
-        // To delete the entire row
-        // .data() is a shortcut to read a data attribute
-        var deleteUrl = $link.data('url');
-        var $row = $link.closest('tr');
-        // Use of the variable self to ensure that in our callback function success, "this" is the object RepLogApp
-        var self = this;
-        $.ajax({
-            url: deleteUrl,
-            method: 'DELETE',
-            success: function () {
-                $row.fadeOut('normal', function () {
-                    $(this).remove();
-                    self.updateTotalWeightLifted();
+        handleRawClick: function () {
+            console.log('Row clicked');
+        }
+        };
 
-                });
-            }
-        });
-    },
+    /**
+     *
+     * A "private" object
+     */
+    var Helper = {
+        initialize: function ($wrapper) {
+            this.$wrapper = $wrapper;
+        },
 
-    handleRawClick: function () {
-        console.log('Row clicked');
-    }
-};
+        calculateTotalWeight: function () {
+            var totalWeight = 0;
+            this.$wrapper.find('tbody tr').each(function () {
+                totalWeight += $(this).data('weight');
+            });
+
+            return totalWeight;
+        }
+    };
+})();
